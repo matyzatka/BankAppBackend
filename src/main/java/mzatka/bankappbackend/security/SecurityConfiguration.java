441d4a96ed2d003375bc4b4dc4840f654e88b580
Matouš Zátka <matous.zatka@seznam.cz>
2022-09-30 18:04:47 +0200

    misc(): few changes

--- src/main/java/mzatka/bankappbackend/security/SecurityConfiguration.java
+++ src/main/java/mzatka/bankappbackend/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,6 +21,11 @@ public class SecurityConfiguration {
 
   public SecurityConfiguration(AuthenticationConfiguration configuration) {
     this.configuration = configuration;
+  }
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder(15);
   }
 
   @Bean
