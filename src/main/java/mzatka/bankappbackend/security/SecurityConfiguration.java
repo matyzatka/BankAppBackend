2f6144203d11042a51087f7582fd89fe2b4e750c
Matouš Zátka <matous.zatka@seznam.cz>
2022-09-30 21:02:20 +0200

    feat(): securing Customer API access for admin

--- src/main/java/mzatka/bankappbackend/security/SecurityConfiguration.java
+++ src/main/java/mzatka/bankappbackend/security/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package mzatka.bankappbackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,8 +40,15 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.csrf().disable();
+    httpSecurity
+        .authorizeRequests()
+        .antMatchers("/api/customer/**")
+        .hasRole("ADMIN")
+        .anyRequest()
+        .authenticated();
     httpSecurity.addFilterBefore(
-            new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     return httpSecurity.build();
   }
 }
