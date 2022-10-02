b5d2c1d448322af8d1d2f700ba7b3f9d57d12957
Matouš Zátka <matous.zatka@seznam.cz>
2022-10-02 19:10:30 +0200

    update(): security config - securing headers

--- src/main/java/mzatka/bankappbackend/security/SecurityConfiguration.java
+++ src/main/java/mzatka/bankappbackend/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -41,6 +42,16 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity.csrf().disable();
+    httpSecurity
+        .headers()
+        .xssProtection()
+        .and()
+        .contentSecurityPolicy("script-src 'self'; form-action 'self'")
+        .and()
+        .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN)
+        .and()
+        .permissionsPolicy(
+            permissionsPolicyConfig -> permissionsPolicyConfig.policy("geolocation=(self)"));
     httpSecurity
         .authorizeRequests()
         .antMatchers("/api/v1/customers/**")
