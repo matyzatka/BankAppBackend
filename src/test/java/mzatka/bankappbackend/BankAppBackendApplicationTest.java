50dbf06fd079868545416880d23f611fd43cbb92
xerx593 <alexlutz79@gmail.com>
2022-10-03 11:23:48 +0200

    adding spring boot (inital smoke) test, fixing https://github.com/spring-projects/spring-framework/issues/29215

--- src/test/java/mzatka/bankappbackend/BankAppBackendApplicationTest.java
+++ src/test/java/mzatka/bankappbackend/BankAppBackendApplicationTest.java
@@ -1,6 +1,20 @@
 package mzatka.bankappbackend;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
-class BankAppBackendApplicationTest {}
+class BankAppBackendApplicationTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    public void testContextLoads() {
+        assertNotNull(context);
+    }
+
+}
