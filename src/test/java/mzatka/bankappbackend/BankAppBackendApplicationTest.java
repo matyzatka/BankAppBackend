1cf1a02c8d3bc2f7fb8e6abe0bf92a06638d380a
Matouš Zátka <matous.zatka@seznam.cz>
2022-10-04 00:02:16 +0200

    update(): contextLoads test

--- src/test/java/mzatka/bankappbackend/BankAppBackendApplicationTest.java
+++ src/test/java/mzatka/bankappbackend/BankAppBackendApplicationTest.java
@@ -1,19 +1,11 @@
 package mzatka.bankappbackend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BankAppBackendApplicationTest {
 
-  @Autowired ApplicationContext context;
-
   @Test
-  public void testContextLoads() {
-    assertNotNull(context);
-  }
+  public void contextLoads() {}
 }
