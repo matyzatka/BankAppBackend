2f23a44a6d8e04ddd02756d2f516e7a9c17e1360
Matouš Zátka <matous.zatka@seznam.cz>
2022-10-03 23:56:43 +0200

    fix(): checkstyle fix

--- src/test/java/mzatka/bankappbackend/BankAppBackendApplicationTest.java
+++ src/test/java/mzatka/bankappbackend/BankAppBackendApplicationTest.java
@@ -1,19 +1,19 @@
 package mzatka.bankappbackend;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BankAppBackendApplicationTest {
 
-    @Autowired
-    ApplicationContext context;
+  @Autowired ApplicationContext context;
 
-    @Test
-    public void testContextLoads() {
-        assertNotNull(context);
-    }
+  @Test
+  public void testContextLoads() {
+    assertNotNull(context);
+  }
 }
