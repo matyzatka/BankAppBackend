27365e266229529ce737e0c14dbc9942061831eb
Matouš Zátka <matous.zatka@seznam.cz>
2022-10-09 21:36:41 +0200

    test(): CustomExceptionHandlerTest

---
+++ src/test/java/mzatka/bankappbackend/advice/CustomExceptionHandlerTest.java
@@ -0,0 +1,68 @@
+package mzatka.bankappbackend.advice;
+
+import mzatka.bankappbackend.exceptions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class CustomExceptionHandlerTest {
+
+  @Autowired private CustomExceptionHandler customExceptionHandler;
+
+  @Test
+  void handleCustomerAlreadyExistsException() {
+    assertNotNull(
+        customExceptionHandler.handleCustomerAlreadyExistsException(
+            new CustomerAlreadyExistsException("msg")));
+  }
+
+  @Test
+  void handleIncorrectPasswordException() {
+    assertNotNull(
+        customExceptionHandler.handleIncorrectPasswordException(
+            new IncorrectPasswordException("msg")));
+  }
+
+  @Test
+  void handleInsufficientFundsException() {
+    assertNotNull(
+        customExceptionHandler.handleInsufficientFundsException(
+            new InsufficientFundsException("msg")));
+  }
+
+  @Test
+  void handleInvalidProductNameException() {
+    assertNotNull(
+        customExceptionHandler.handleInvalidProductNameException(
+            new InvalidProductNameException("msg")));
+  }
+
+  @Test
+  void handleNoSuchCustomerException() {
+    assertNotNull(
+        customExceptionHandler.handleNoSuchCustomerException(new NoSuchCustomerException("msg")));
+  }
+
+  @Test
+  void handleNoSuchProductWithIbanException() {
+    assertNotNull(
+        customExceptionHandler.handleNoSuchProductWithIbanException(
+            new NoSuchProductWithIbanException("msg")));
+  }
+
+  @Test
+  void handleUnauthorizedAccountUsageException() {
+    assertNotNull(
+        customExceptionHandler.handleUnauthorizedAccountUsageException(
+            new UnauthorizedAccountUsageException("msg")));
+  }
+
+  @Test
+  void handleUnknownErrorException() {
+    assertNotNull(
+        customExceptionHandler.handleUnknownErrorException(new UnknownErrorException("msg")));
+  }
+}
